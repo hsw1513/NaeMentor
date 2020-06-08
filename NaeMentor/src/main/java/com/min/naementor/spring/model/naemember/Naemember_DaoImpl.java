279842830d2f87a7623785790246a1b6563b38d3
hsw1513@naver.com <hsw1513@naver.com>
2020-06-09 08:17:55 +0900

    날짜: 2020.06.09. 작성자: 황상우 내용: 회원관리 기본 세팅 완료

---
+++ NaeMentor/src/main/java/com/min/naementor/spring/model/naemember/Naemember_DaoImpl.java
@@ -0,0 +1,5 @@
+package com.min.naementor.spring.model.naemember;
+
+public class Naemember_DaoImpl implements Naemember_IDao {
+
+}
---
+++ NaeMentor/src/main/java/com/min/naementor/spring/model/naemember/Naemember_IService.java
@@ -0,0 +1,5 @@
+package com.min.naementor.spring.model.naemember;
+
+public interface Naemember_IService {
+
+}
