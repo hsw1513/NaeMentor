0bc6e5282a7294ea8ba0d4ecbde7f3628e112fed
hsw1513@naver.com <hsw1513@naver.com>
2020-06-08 11:19:58 +0900

    Date: 2020. 06. 08. Write: 황상우 Content: Dto생성(NaeMember, Profile, AttachFile)

---
+++ NaeMentor/src/main/java/com/min/naementor/dtos/NaememberDto.java
@@ -0,0 +1,217 @@
+package com.min.naementor.dtos;
+
+import java.io.Serializable;
+
+public class NaememberDto implements Serializable {
+	                            
+	
+	private static final long serialVersionUID = -6310426503118340175L;
+	
+	private String memberseq     ;
+	private String email         ;
+	private String password      ;
+	private String nickname      ;
+	private String introduce     ;
+	private String phone         ;
+	private String birthday      ;
+	private String gender        ;
+	private String auth          ;
+	private String userstatus    ;
+	private String mentortier    ;
+	private String logincnt      ;
+	private String reportcnt     ;
+	private String joindate      ;
+	private String menteecnt     ;
+	private String menteeaccstar ;
+	private String lastaccess    ;
+	private String byebye        ;
+	
+	public NaememberDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NaememberDto(String memberseq, String email, String password, String nickname, String introduce,
+			String phone, String birthday, String gender, String auth, String userstatus, String mentortier,
+			String logincnt, String reportcnt, String joindate, String menteecnt, String menteeaccstar,
+			String lastaccess, String byebye) {
+		super();
+		this.memberseq = memberseq;
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.introduce = introduce;
+		this.phone = phone;
+		this.birthday = birthday;
+		this.gender = gender;
+		this.auth = auth;
+		this.userstatus = userstatus;
+		this.mentortier = mentortier;
+		this.logincnt = logincnt;
+		this.reportcnt = reportcnt;
+		this.joindate = joindate;
+		this.menteecnt = menteecnt;
+		this.menteeaccstar = menteeaccstar;
+		this.lastaccess = lastaccess;
+		this.byebye = byebye;
+	}
+
+	public String getMemberseq() {
+		return memberseq;
+	}
+
+	public void setMemberseq(String memberseq) {
+		this.memberseq = memberseq;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getIntroduce() {
+		return introduce;
+	}
+
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+
+	public String getUserstatus() {
+		return userstatus;
+	}
+
+	public void setUserstatus(String userstatus) {
+		this.userstatus = userstatus;
+	}
+
+	public String getMentortier() {
+		return mentortier;
+	}
+
+	public void setMentortier(String mentortier) {
+		this.mentortier = mentortier;
+	}
+
+	public String getLogincnt() {
+		return logincnt;
+	}
+
+	public void setLogincnt(String logincnt) {
+		this.logincnt = logincnt;
+	}
+
+	public String getReportcnt() {
+		return reportcnt;
+	}
+
+	public void setReportcnt(String reportcnt) {
+		this.reportcnt = reportcnt;
+	}
+
+	public String getJoindate() {
+		return joindate;
+	}
+
+	public void setJoindate(String joindate) {
+		this.joindate = joindate;
+	}
+
+	public String getMenteecnt() {
+		return menteecnt;
+	}
+
+	public void setMenteecnt(String menteecnt) {
+		this.menteecnt = menteecnt;
+	}
+
+	public String getMenteeaccstar() {
+		return menteeaccstar;
+	}
+
+	public void setMenteeaccstar(String menteeaccstar) {
+		this.menteeaccstar = menteeaccstar;
+	}
+
+	public String getLastaccess() {
+		return lastaccess;
+	}
+
+	public void setLastaccess(String lastaccess) {
+		this.lastaccess = lastaccess;
+	}
+
+	public String getByebye() {
+		return byebye;
+	}
+
+	public void setByebye(String byebye) {
+		this.byebye = byebye;
+	}
+
+	@Override
+	public String toString() {
+		return "NaememberDto [memberseq=" + memberseq + ", email=" + email + ", password=" + password + ", nickname="
+				+ nickname + ", introduce=" + introduce + ", phone=" + phone + ", birthday=" + birthday + ", gender="
+				+ gender + ", auth=" + auth + ", userstatus=" + userstatus + ", mentortier=" + mentortier
+				+ ", logincnt=" + logincnt + ", reportcnt=" + reportcnt + ", joindate=" + joindate + ", menteecnt="
+				+ menteecnt + ", menteeaccstar=" + menteeaccstar + ", lastaccess=" + lastaccess + ", byebye=" + byebye
+				+ "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ NaeMentor/src/main/java/com/min/naementor/dtos/ProfileDto.java
@@ -0,0 +1,121 @@
+package com.min.naementor.dtos;
+
+import java.io.Serializable;
+
+public class ProfileDto implements Serializable {
+	
+	private static final long serialVersionUID = -2992432246437659373L;
+	
+	private String memberseq   ;
+	private String photo       ;
+	private String school      ;
+	private String major       ;
+	private String career      ;
+	private String certificate ;
+	private String specialfield;
+	private String pmdate      ;
+	private String mentorcnt   ;
+	private String mentoaccstar;
+	private String filechk     ;
+	
+	public ProfileDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ProfileDto(String memberseq, String photo, String school, String major, String career, String certificate,
+			String specialfield, String pmdate, String mentorcnt, String mentoaccstar, String filechk) {
+		super();
+		this.memberseq = memberseq;
+		this.photo = photo;
+		this.school = school;
+		this.major = major;
+		this.career = career;
+		this.certificate = certificate;
+		this.specialfield = specialfield;
+		this.pmdate = pmdate;
+		this.mentorcnt = mentorcnt;
+		this.mentoaccstar = mentoaccstar;
+		this.filechk = filechk;
+	}
+	
+	public String getMemberseq() {
+		return memberseq;
+	}
+	public void setMemberseq(String memberseq) {
+		this.memberseq = memberseq;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getCareer() {
+		return career;
+	}
+	public void setCareer(String career) {
+		this.career = career;
+	}
+	public String getCertificate() {
+		return certificate;
+	}
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+	public String getSpecialfield() {
+		return specialfield;
+	}
+	public void setSpecialfield(String specialfield) {
+		this.specialfield = specialfield;
+	}
+	public String getPmdate() {
+		return pmdate;
+	}
+	public void setPmdate(String pmdate) {
+		this.pmdate = pmdate;
+	}
+	public String getMentorcnt() {
+		return mentorcnt;
+	}
+	public void setMentorcnt(String mentorcnt) {
+		this.mentorcnt = mentorcnt;
+	}
+	public String getMentoaccstar() {
+		return mentoaccstar;
+	}
+	public void setMentoaccstar(String mentoaccstar) {
+		this.mentoaccstar = mentoaccstar;
+	}
+	public String getFilechk() {
+		return filechk;
+	}
+	public void setFilechk(String filechk) {
+		this.filechk = filechk;
+	}
+	
+	@Override
+	public String toString() {
+		return "ProfileDto [memberseq=" + memberseq + ", photo=" + photo + ", school=" + school + ", major=" + major
+				+ ", career=" + career + ", certificate=" + certificate + ", specialfield=" + specialfield + ", pmdate="
+				+ pmdate + ", mentorcnt=" + mentorcnt + ", mentoaccstar=" + mentoaccstar + ", filechk=" + filechk + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
