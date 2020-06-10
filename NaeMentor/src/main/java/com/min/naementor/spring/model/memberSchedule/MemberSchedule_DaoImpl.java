cd05b2958ccf7f807a4da930869277d6c9109b72
gusdnsladl@naver.com <gusdnsladl@naver.com>
2020-06-10 17:50:11 +0900

    날짜: 2020. 06. 10. 작성자: 구현우 내용: 후기게시판 외 완료

--- NaeMentor/src/main/java/com/min/naementor/spring/model/memberSchedule/MemberSchedule_DaoImpl.java
+++ NaeMentor/src/main/java/com/min/naementor/spring/model/memberSchedule/MemberSchedule_DaoImpl.java
@@ -1,25 +1,30 @@
 package com.min.naementor.spring.model.memberSchedule;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.min.naementor.dtos.MemberScheduleDto;
-
+@Repository
 public class MemberSchedule_DaoImpl implements MemberSchedule_IDao{
-
+	
+	@Autowired
+	private SqlSessionTemplate session;
+	private final String NS ="com.min.naementor.memberSchedule.";
+	
 	@Override
 	public boolean insertSchedule(MemberScheduleDto dto) {
-		// TODO Auto-generated method stub
-		return false;
+		return session.insert(NS+"insertSchedule", dto)>0?true:false;
 	}
 
 	@Override
 	public boolean modifySchedule(MemberScheduleDto dto) {
-		// TODO Auto-generated method stub
-		return false;
+		return session.update(NS+"modifySchedule", dto)>0?true:false;
 	}
 
 	@Override
 	public boolean deleteSchedule(String boardseq) {
-		// TODO Auto-generated method stub
-		return false;
+		return session.delete(NS+"deleteSchedule", boardseq)>0?true:false;
 	}
 
 }
