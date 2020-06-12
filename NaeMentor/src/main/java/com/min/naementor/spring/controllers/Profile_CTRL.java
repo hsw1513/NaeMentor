1a2c7da114ddbb16a890b511ad0a2e7baa37c05f
gusdnsladl@naver.com <gusdnsladl@naver.com>
2020-06-12 14:29:08 +0900

    날짜: 2020. 06. 12. 작성자: 구현우 내용: DB서버 변경

--- NaeMentor/src/main/java/com/min/naementor/spring/controllers/Profile_CTRL.java
+++ NaeMentor/src/main/java/com/min/naementor/spring/controllers/Profile_CTRL.java
@@ -1,12 +1,21 @@
 package com.min.naementor.spring.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.min.naementor.dtos.NaememberDto;
+
 @Controller
 public class Profile_CTRL {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	// 프로필 페이지로 이동
 	@RequestMapping("/Profile.do")
 	public String profile(Model model) {
 		return "Profile/Profile";
